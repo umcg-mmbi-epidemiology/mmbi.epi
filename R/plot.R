e16cab60341a4164731f76df8ed68a2225220d82
Matthijs Berends <m.s.berends@umcg.nl>
2025-08-26 15:56:11 +0200

    cleaner plot style registration

--- R/plot.R
+++ R/plot.R
@@ -16,52 +16,82 @@
 #  useful, but it comes WITHOUT ANY WARRANTY OR LIABILITY.              #
 # ===================================================================== #
 
-#' UMCG Scale Colours
+#' UMCG Plot Style
 #'
 #' These are `ggplot2` helpers to add organisational colours to `ggplot` objects.
 #' @param n Number of colours required.
 #' @param ... arguments passed on to [ggplot2::scale_colour_manual()] or [ggplot2::scale_fill_manual()]
-#' @rdname scale_colour_umcg
+#' @rdname plot_style_umcg
 #' @importFrom plot2 get_colour
 #' @export
 #' @examples
 #' library(ggplot2)
 #'
-#' mtcars |>
+#' p <- mtcars |>
 #'   ggplot(aes(x = hp,
 #'              y = mpg,
 #'              colour = as.factor(vs)),
 #'              fill = as.factor(vs)) +
 #'   geom_point(size = 3) +
-#'   scale_colour_umcg(n = 3)
+#'   geom_smooth()
+#' p
 #'
-#' mtcars |>
-#'   ggplot(aes(x = hp,
-#'              y = mpg,
-#'              colour = as.factor(vs),
-#'              fill = as.factor(vs))) +
-#'   geom_point() +
-#'   geom_smooth(method="lm") +
-#'   scale_colour_umcg(n = 2) +
+#' p +
+#'   plot_style_umcg(n = 2) +
 #'   scale_fill_umcg(n = 2) +
-#'   labs(title = "Non-UMCG-styled plot")
+#'   labs(title = "UMCG colours")
 #'
 #' # With plot2(), this all goes automatically with less code
 #' mtcars |>
-#'   plot2(hp, mpg, as.factor(vs),
-#'         title = "UMCG-styled plot",
-#'         subtitle = "(with the right font too; 'Outfit' from Google Fonts)") |>
-#'   add_smooth(method = "lm")
+#'   plot2(x = hp,
+#'         y = mpg,
+#'         category = as.factor(vs),
+#'         smooth = TRUE,
+#'         title = "Fully UMCG-styled plot",
+#'         subtitle = "(with the right font too; 'Outfit' from Google Fonts)")
 scale_colour_umcg <- function(..., n) {
   ggplot2::scale_colour_manual(values = get_colour("umcg", length = n), ...)
 }
 
-#' @rdname scale_colour_umcg
+#' @rdname plot_style_umcg
 #' @export
 scale_fill_umcg <- function(..., n) {
   ggplot2::scale_fill_manual(values = get_colour("umcg", length = n), ...)
 }
 
+#' @rdname plot_style_umcg
+#' @export
+register_umcg_plot_style <- function() {
+  # register UMCG colours to plot2
+  suppressMessages(register_colour(
+    umcgblauw        = "#003183",
+    umcgdonkerblauw  = "#003183",
+    umcglichtblauw   = "#eef7fb",
+    umcgoranje       = "#FF7D00",
+    umcgdonkeroranje = "#FF7D00",
+    umcglichtoranje  = "#fff2e4",
+    umcgteal         = "#007A8A", # from here on non-brand, UMCG only provides 2 colours with good intensity :(
+    umcggroen        = "#2E8540",
+    umcgpaars        = "#5B3FA8",
+    umcggrijs        = "#3A3A3A",
+    umcggoud         = "#B38F00",
+    umcg             = c("umcgblauw","umcgoranje","umcgteal",
+                         "umcggroen","umcgpaars","umcggrijs","umcggoud")))
+  options(plot2.colour = "umcg",
+          plot2.colour_font_secondary = "umcgdonkerblauw",
+          plot2.colour_sf_fill = c("umcglichtblauw", "umcgdonkerblauw"),
+          plot2.font = "Outfit") # 'Outfit', a Google Font, is the official UMCG font
+}
+
+#' @rdname plot_style_umcg
+#' @export
+unregister_umcg_plot_style <- function() {
+  suppressMessages(unregister_colour("^umcg"))
+  options(plot2.colour = NULL,
+          plot2.colour_font_secondary = NULL,
+          plot2.colour_sf_fill = NULL,
+          plot2.font = NULL)
+}
 
 # These are re-exports; they will link to the original package and help file --------
 
--- R/zzz.R
+++ R/zzz.R
@@ -24,32 +24,10 @@
   # - `certegis` GIS functions can be used in other packages (`vctrs` pkg will otherwise complain)
   requireNamespace("sf", quietly = TRUE)
 
-  # register UMCG colours to plot2
-  suppressMessages(register_colour(
-    umcgblauw        = "#003183",
-    umcgdonkerblauw  = "#003183",
-    umcglichtblauw   = "#eef7fb",
-    umcgoranje       = "#FF7D00",
-    umcgdonkeroranje = "#FF7D00",
-    umcglichtoranje  = "#fff2e4",
-    umcgteal         = "#007A8A", # from here on non-brand, UMCG only provides 2 colours with good intensity :(
-    umcggroen        = "#2E8540",
-    umcgpaars        = "#5B3FA8",
-    umcggrijs        = "#3A3A3A",
-    umcggoud         = "#B38F00",
-    umcg             = c("umcgblauw","umcgoranje","umcgteal",
-                        "umcggroen","umcgpaars","umcggrijs","umcggoud")))
-  options(plot2.colour = "umcg",
-          plot2.colour_font_secondary = "umcgdonkerblauw",
-          plot2.colour_sf_fill = c("umcglichtblauw", "umcgdonkerblauw"),
-          plot2.font = "Outfit") # 'Outfit', a Google Font, is the official UMCG font
+  register_umcg_plot_style()
 }
 
 #' @importFrom plot2 unregister_colour
 .onUnload <- function(...) {
-  suppressMessages(unregister_colour("^umcg"))
-  options(plot2.colour = NULL,
-          plot2.colour_font_secondary = NULL,
-          plot2.colour_sf_fill = NULL,
-          plot2.font = NULL)
+  unregister_umcg_plot_style()
 }
