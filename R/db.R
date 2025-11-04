f431611147b8e271cfdc590f10541d341e64ac9b
Matthijs Berends <m.s.berends@umcg.nl>
2025-11-04 23:33:01 +0100

    fix join fn

--- R/db.R
+++ R/db.R
@@ -16,7 +16,7 @@
 #  useful, but it comes WITHOUT ANY WARRANTY OR LIABILITY.              #
 # ===================================================================== #
 
-#' Database Access
+#' GLIMS Database Access
 #'
 #' Establishes a connection to the GLIMS Oracle database using environment variables for all configuration parameters. This function is designed for use in secure environments such as Posit Workbench, where credentials and driver paths are stored outside the source code.
 #' @section Environment Variables:
@@ -89,7 +89,7 @@
 #' }
 connect_db <- function() {
   dbConnect(
-    odbc(),
+    odbc::odbc(),
     Driver   = Sys.getenv("MMBI_EPIDS_DRIVER"),
     Host     = Sys.getenv("MMBI_EPIDS_HOST"),
     Port     = Sys.getenv("MMBI_EPIDS_PORT"),
@@ -98,46 +98,76 @@ connect_db <- function() {
     PWD      = Sys.getenv("MMBI_EPIDS_PASS"))
 }
 
-#' @param con Connection object.
-#' @param table_name Name of the table to query.
-#' @param prefix Prefix string to prepend to `table_name` internally.
-#' @rdname db
-#' @importFrom dplyr tbl
-#' @export
-glims_tbl <- function(con, table_name, prefix = "ORAGLIMS") {
-  con |> tbl(I(paste0(prefix, ".", table_name)))
-}
-
-#' @param .data SQL data object.
-#' @param type Direction of the join, defaults to `"left"`.
-#' * [`"inner"`][dplyr::inner_join()]: returns matched x rows.
-#' * [`"left"`][dplyr::left_join()]: returns all x rows.
-#' * [`"right"`][dplyr::right_join()]: returns matched of x rows, followed by unmatched y rows.
-#' * [`"full"`][dplyr::full_join()]: returns all x rows, followed by unmatched y rows.
-#' @param by Column name(s) to join by. Can be a named vector to allow different names:\cr  `by = c("col_A" = "col_B")`.
-#' @param ... Arguments passed on the join functions.
-#' @rdname db
-#' @importFrom dplyr left_join right_join full_join inner_join
-#' @export
-glims_join_tbl <- function(.data, table_name, by = NULL, ..., type = "left", prefix = "ORAGLIMS") {
-  con <- .data[["src"]]$con
-  table_y <- con |> tbl(I(paste0(prefix, ".", table_name)))
-  if (type == "left") {
-    .data |> left_join(table_y, ...)
-  } else if (type == "right") {
-    .data |> right_join(table_y, ...)
-  } else if (type == "full") {
-    .data |> full_join(table_y, ...)
-  } else if (type == "inner") {
-    .data |> inner_join(table_y, ...)
-  } else {
-    stop("Only supported joins: left, right, full, inner")
-  }
-}
-
 #' @rdname db
 #' @importFrom odbc dbDisconnect
 #' @export
 disconnect_db <- function(con) {
   dbDisconnect(con)
 }
+
+#' @param con Connection object.
+#' @param table_name Name of the table to query.
+#' @param schema Database schema; the upper structure name of `table_name`.
+#' @rdname db
+#' @importFrom dplyr tbl
+#' @export
+glims_tbl <- function(con, table_name, schema = "ORAGLIMS") {
+  con |> tbl(I(paste0(schema, ".", table_name)))
+}
+
+#' @param .data SQL data object.
+#' @param by Column name(s) to join by. Can be a named vector to allow different names:\cr  `by = c("col_A" = "col_B")`.
+#' @param ... Arguments passed on the join functions.
+#' @param cols_to_keep Columns to keep of the joined column. Columns used in `by` will always be included. Supports [tidyselect language][tidyselect::starts_with()].
+#' @param type Direction of the join, defaults to `"left"`.
+#' * [`"inner"`][dplyr::inner_join()]: returns matched x rows.
+#' * [`"left"`][dplyr::left_join()]: returns all x rows.
+#' * [`"right"`][dplyr::right_join()]: returns matched of x rows, followed by unmatched y rows.
+#' * [`"full"`][dplyr::full_join()]: returns all x rows, followed by unmatched y rows.
+#' @rdname db
+#' @importFrom dplyr left_join right_join full_join inner_join select any_of collect
+#' @export
+glims_join_tbl <- function(.data,
+                           table_name,
+                           by = NULL,
+                           ...,
+                           cols_to_keep = NULL,
+                           type = "left",
+                           schema = "ORAGLIMS") {
+  con <- .data[["src"]]$con
+
+  if (interactive()) {
+    message("Joining table '", table_name, "'...", appendLF = FALSE)
+  }
+
+  table_y <- con |> glims_tbl(table_name, schema)
+
+  if (tryCatch(!is.null(cols_to_keep), error = function(e) TRUE)) {
+    keep_cols <- table_y |> utils::head(0) |> collect() |> select({{ cols_to_keep }}) |> colnames()
+    table_y <- table_y |> select(any_of(c(names(by), unname(by), keep_cols)))
+  }
+
+  if (type == "left") {
+    out <- .data |> left_join(table_y, by = by, ...)
+  } else if (type == "right") {
+    out <- .data |> right_join(table_y, by = by, ...)
+  } else if (type == "full") {
+    out <- .data |> full_join(table_y, by = by, ...)
+  } else if (type == "inner") {
+    out <- .data |> inner_join(table_y, by = by, ...)
+  } else {
+    stop("Only supported joins: left, right, full, inner")
+  }
+
+  if (interactive()) {
+    message("OK")
+  }
+
+  out
+}
+
+
+# get_glims_results <- function(con, ...) {
+#   con |>
+#
+# }
